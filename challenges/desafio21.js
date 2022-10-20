8d5bc9dcd5b86b642ab7572c75cf4603a05a5a51
Isabela Rozani Floriano <irf161295@gmail.com>
2022-10-20 17:52:35 -0300

    feat: create request to remove the last ingrediente from 'Cheddar McMelt'

---
+++ challenges/desafio21.js
@@ -0,0 +1,12 @@
+db.produtos.updateOne({ 
+  nome: { 
+    $in: ["Cheddar McMelt"],
+    },
+}, {
+  $pop: {
+    ingredientes: 1,
+  },
+}); 
+db.produtos.find({ }, {
+  nome: 1, ingredientes: 1, _id: 0, 
+});
